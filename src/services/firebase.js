9e670d7546dc90e2cb457513d796c5dc7919aeac
Michael Knott <knottweb@gmail.com>
2021-03-31 21:13:19 +0100

    firebase code

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,7 @@ import firebase from "firebase";
 const config = {
   apiKey: "AIzaSyBfB7FibOJrWZe69Pj2fIRKC2WHq-OVqR8",
   authDomain: "partify-c7a0a.firebaseapp.com",
-  databaseURL: "ADD-YOUR-DETAILS-HERE",
+  databaseURL: "https://partify-c7a0a-default-rtdb.firebaseio.com/",
 };
 firebase.initializeApp(config);
 export const auth = firebase.auth;
