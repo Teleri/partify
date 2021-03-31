302e7180b73c4a79b099d103b89a5c66cfa17ee4
Michael Knott <knottweb@gmail.com>
2021-03-31 21:13:09 +0100

    App.js code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-const config = {
-  apiKey: "ADD-YOUR-DETAILS-HERE",
-  authDomain: "ADD-YOUR-DETAILS-HERE",
-  databaseURL: "ADD-YOUR-DETAILS-HERE",
-};
-firebase.initializeApp(config);
-export const auth = firebase.auth;
-export const db = firebase.database();
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { auth } from "./services/firebase";
+import landingPage from "./containers/landingPage/landingPage";
+import signUpPage from "./containers/signUpPage/signUpPage";
+import theRoomPage from "./containers/theRoomPage/theRoomPage";
 
-class App extends React.Component {
-  state = {};
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/chat" />
+        )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  state = {
+    authenticated: false,
+    loading: true,
+  };
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    });
+  }
+
   render() {
-    return <div>Hello world</div>;
+    return this.state.loading === true ? (
+      <div>
+        <span>Loading...</span>
+      </div>
+    ) : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={landingPage} />
+          <PrivateRoute
+            path="/chat"
+            authenticated={this.state.authenticated}
+            component={theRoomPage}
+          />
+          <PublicRoute
+            path="/signup"
+            authenticated={this.state.authenticated}
+            component={signUpPage}
+          />
+        </Switch>
+      </Router>
+    );
   }
 }
 
