8fd6d9fbb98bc763b248253f12ae772aeeb1758d
Michael Knott <knottweb@gmail.com>
2021-04-08 13:15:40 +0100

    add sass index and basic styling for navbar

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { auth } from "../../services/firebase";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import "../../styles/shared/navbar.scss";
 
 function Navbar() {
   return (
     <header>
       <Router>
         <nav>
-          <Link to="/">Home</Link>
-          <div>
-            {auth().currentUser ? (
-              <div>
-                <Link to="/chat">Chat</Link>
-                <button onClick={() => auth().signOut()}>Logout</button>
-              </div>
-            ) : (
-              <div>
-                <Link to="/login">Sign In</Link>
-                <Link to="/signup">Sign Up</Link>
-              </div>
-            )}
+          <div className="navbar-container">
+            <div className="nav-links">
+              <Link to="/">Home</Link>
+            </div>
+            <div>
+              {auth().currentUser ? (
+                <div className="logged-in-links">
+                  <div className="nav-links">
+                    <Link to="/chat">Chat</Link>
+                  </div>
+                  <div className="nav-links">
+                    <button onClick={() => auth().signOut()}>Logout</button>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <Link to="/login">Sign In</Link>
+                  <Link to="/signup">Sign Up</Link>
+                </div>
+              )}
+            </div>
           </div>
         </nav>
       </Router>
